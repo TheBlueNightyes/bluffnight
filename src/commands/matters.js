f24b4ac6ddc05580ddbdc48da6d417061542b18d
TheBlueNightyes <thebluenightyes@gmail.com>
2026-03-14 17:12:03 -0700

    Initial commit

---
+++ src/commands/matters.js
@@ -0,0 +1,27 @@
+import { createCommandConfig, logger } from 'robo.js'
+
+export const config = createCommandConfig({
+	description: 'I LOVE U YUJI!!!',
+    options: [{
+		name: 'question',
+		description: 'he decides',
+		type: 'string',
+		required: true
+	}]
+})
+
+export default (interaction) => {
+	logger.info(`Lazy's actions discovered by ${interaction.user}`)
+    const question = interaction.options.getString('question');
+	const image = [
+		'./src/assets/DoesItMatter.jpg',
+        './src/assets/IDontCare.png',
+		'./src/assets/IDontCareYuji.png',
+        './src/assets/ItDoesntMatter.png',
+        './src/assets/ItMatters.jpg',
+        './src/assets/Leash.jpg'
+	];
+
+	const chosenImage = image[Math.floor(Math.random() * image.length)];
+	interaction.reply(`> ${question}\n${chosenImage}`)
+}
