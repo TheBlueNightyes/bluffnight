c8bed16125618328c454bf893098a9e05d5732f8
TheBlueNightyes <thebluenightyes@gmail.com>
2026-03-14 17:14:05 -0700

    Initial commit

--- src/commands/matters.js
+++ src/commands/matters.js
@@ -1,27 +1,32 @@
-import { createCommandConfig, logger } from 'robo.js'
+import { createCommandConfig, logger } from 'robo.js';
+import { AttachmentBuilder } from 'discord.js';
+import path from 'path';
 
 export const config = createCommandConfig({
-	description: 'I LOVE U YUJI!!!',
+    description: 'I LOVE U YUJI!!!',
     options: [{
-		name: 'question',
-		description: 'he decides',
-		type: 'string',
-		required: true
-	}]
-})
+        name: 'question',
+        description: 'he decides',
+        type: 'string',
+        required: true
+    }]
+});
 
 export default (interaction) => {
     logger.info(`Matters command used by ${interaction.user.tag}`);
+
     const question = interaction.options.getString('question');
-	const image = [
-		'./src/assets/DoesItMatter.jpg',
+    const images = [
+        './src/assets/DoesItMatter.jpg',
         './src/assets/IDontCare.png',
-		'./src/assets/IDontCareYuji.png',
+        './src/assets/IDontCareYuji.png',
         './src/assets/ItDoesntMatter.png',
         './src/assets/ItMatters.jpg',
         './src/assets/Leash.jpg'
-	];
+    ];
 
-	const chosenImage = image[Math.floor(Math.random() * image.length)];
-	interaction.reply(`> ${question}\n${chosenImage}`)
-}
+    const chosenImage = images[Math.floor(Math.random() * images.length)];
+    const attachment = new AttachmentBuilder(path.resolve(chosenImage));
+
+    interaction.reply({ content: `> ${question}`, files: [attachment] });
+};
